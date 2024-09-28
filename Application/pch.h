b64fa957fb3e45e666c3dc292ce4b545fa1f30ba
Jin-SukKim <99wlstjr@gmail.com>
2024-09-28 18:16:48 +0900

    Project Setting

---
+++ Application/pch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifdef _DEBUG
+#pragma comment(lib, "Math\\Debug\\Math.lib")
+#pragma comment(lib, "Renderer\\Debug\\Renderer.lib")
+#pragma comment(lib, "Engine\\Debug\\Engine.lib")
+#else
+#pragma comment(lib, "Math\\Release\\Math.lib")
+#pragma comment(lib, "Renderer\\Release\\Renderer.lib")
+#pragma comment(lib, "Engine\\Release\\Engine.lib")
+#endif
+
+#include <Windows.h>
+#include <vector>
+#include <memory>
